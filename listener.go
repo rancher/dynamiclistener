8f13b193a1028827453c064efa89629725b6cdc7
Jonas Wagner <knoppiks@users.noreply.github.com>
2023-07-14 08:16:08 +0200

    Use more Verbose name for Listener
    Co-authored-by: Brad Davidson <brad@oatmail.org>
    Signed-off-by: Jonas Wagner <jwagner@knoppiks.de

--- listener.go
+++ listener.go
@@ -34,12 +34,12 @@ type SetFactory interface {
 	SetFactory(tls TLSFactory)
 }
 
-// Deprecated: Use NewListener2 instead as it supports intermediate CAs
+// Deprecated: Use NewListenerWithChain instead as it supports intermediate CAs
 func NewListener(l net.Listener, storage TLSStorage, caCert *x509.Certificate, caKey crypto.Signer, config Config) (net.Listener, http.Handler, error) {
-	return NewListener2(l, storage, []*x509.Certificate{caCert}, caKey, config)
+	return NewListenerWithChain(l, storage, []*x509.Certificate{caCert}, caKey, config)
 }
 
-func NewListener2(l net.Listener, storage TLSStorage, caCert []*x509.Certificate, caKey crypto.Signer, config Config) (net.Listener, http.Handler, error) {
+func NewListenerWithChain(l net.Listener, storage TLSStorage, caCert []*x509.Certificate, caKey crypto.Signer, config Config) (net.Listener, http.Handler, error) {
 	if config.CN == "" {
 		config.CN = "dynamic"
 	}
